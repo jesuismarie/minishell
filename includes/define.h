ac6df48a5d1b5b1d73369a54dc70c86fb658c736
kali-linux <mari.nazaryan7173@gmail.com>
2024-01-19 06:45:32 -0600

    includes

---
+++ includes/define.h
@@ -0,0 +1,175 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   define.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnazarya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/03 15:59:04 by mnazarya          #+#    #+#             */
+/*   Updated: 2024/01/19 03:56:02 by mnazarya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DEFINE_H
+# define DEFINE_H
+
+# define ERR_CD		"minishell: cd: HOME not set 🏠\n"
+# define CD_2MUCH	"minishell: cd: too many arguments\n"
+# define ERR_EXIT	"minishell: exit: too many arguments\n"
+# define ERR_HIST	"minishell: history: too many arguments\n"
+# define ERR_EXPORT	"minishell: export:"
+# define ERR_UNSET	"minishell: unset:"
+# define ERR_MSG	"minishell: syntax error near unexpected token `"
+# define ERR_OP_B	"minishell: syntax error near unexpected token `('\n"
+# define ERR_CL_B	"minishell: syntax error near unexpected token `)'\n"
+# define ERR_AND	"minishell: syntax error near unexpected token `&'\n"
+# define ERR_NL		"minishell: syntax error near unexpected token `newline'\n"
+# define ERR_LIM	"here-document delimited by end-of-file (wanted `"
+# define ERR_EOF	"minishell: unexpected EOF while looking for matching `"
+# define ERR_SYN	"minishell: syntax error: unexpected end of file\n"
+# define PERROR_MSG	"minishell"
+# define HERE_MAX	"minishell: maximum here-document count exceeded\n"
+# define SQ_ERR		"minishell: unclosed \' quote 🙃\n"
+# define DQ_ERR		"minishell: unclosed \" quote 🙃\n"
+
+# define EQUAL		'='
+# define OR_IF		"||"
+# define AND_IF		"&&"
+# define LESS		'<'
+# define GREAT		'>'
+# define DLESS		"<<"
+# define DGREAT		">>"
+# define DOLLAR		'$'
+# define PIPE		'|'
+# define LBRACE		'('
+# define RBRACE		')'
+# define SQUOTES	'\''
+# define DQUOTES	'\"'
+# define WILDCARD	'*'
+
+# define PS			"minishell-4.2$ "
+
+# define OPERATORS	"()<>|&"
+
+typedef enum e_token_type
+{
+	HEREDOC,
+	APPEND,
+	FILE_IN,
+	FILE_OUT,
+	ENV_PARAM,
+	PIPE_OP,
+	AND_OP,
+	OR_OP,
+	BRACE_OPEN,
+	BRACE_CLOSE,
+	WORD,
+	ERROR
+}	t_token_type;
+
+typedef enum e_flags
+{
+	F_DOLLAR			= 1 << 0,
+	F_SQUOTES			= 1 << 1,
+	F_DQUOTES			= 1 << 2,
+	F_ASSIGNMENT		= 1 << 3,
+	F_MUL_DOLLAR		= 1 << 4
+}	t_flags;
+
+typedef enum e_ast_node_type
+{
+	AST_LOGICAL_OP,
+	AST_REDIRECTION,
+	AST_PIPE,
+	AST_COMMAND
+}	t_ast_node_type;
+
+typedef struct s_stack
+{
+	char			c;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}	t_stack;
+
+typedef struct s_input
+{
+	int				flag;
+	char			*input;
+	struct s_input	*next;
+	struct s_input	*prev;
+}	t_input;
+
+typedef struct s_token
+{
+	t_token_type	type;
+	t_input			*cmd;
+	struct s_token	*prev;
+	struct s_token	*next;
+}	t_token;
+
+typedef struct s_ast_node
+{
+	t_ast_node_type		type;
+	int					subshell_flag;
+	void				*node;
+	struct s_ast_node	*next;
+	struct s_ast_node	*prev;
+}	t_ast_node;
+
+typedef struct s_pipe
+{
+	int			in_fd;
+	int			out_fd;
+	t_ast_node	*left;
+	t_ast_node	*right;
+}	t_pipe;
+
+typedef struct s_redir
+{
+	int				in_fd;
+	int				out_fd;
+	t_token_type	type;
+	struct s_redir	*next;
+	t_input			*filename;
+}	t_redir;
+
+typedef struct s_operator
+{
+	t_token_type	type;
+	t_ast_node		*left;
+	t_ast_node		*right;
+}	t_operator;
+
+typedef struct s_cmd
+{
+	int		in_fd;
+	int		out_fd;
+	t_input	*name;
+	t_input	*args;
+}	t_cmd;
+
+typedef struct s_env
+{
+	char			*var_name;
+	char			*var_value;
+	int				hidden;
+	struct s_env	*next;
+	struct s_env	*prev;
+}	t_env;
+
+typedef struct s_shell
+{
+	int			err;
+	char		**env;
+	char		*line;
+	t_ast_node	*tree;
+	char		*hist;
+	int			ex_code;
+	t_env		*env_lst;
+	char		*err_msg;
+	int			history_fd;
+	t_token		*token_head;
+	int			all_fds[FOPEN_MAX];
+}	t_shell;
+
+#endif
---
+++ includes/minishell.h
@@ -0,0 +1,155 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnazarya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 00:23:21 by mnazarya          #+#    #+#             */
+/*   Updated: 2024/01/19 03:45:59 by mnazarya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <libft.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <limits.h>
+# include <signal.h>
+# include <define.h>
+# include <stdint.h>
+# include <dirent.h>
+# include <termios.h>
+# include <sys/wait.h>
+# include <get_next_line.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+
+extern int	g_stat;
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------PRINT-------------------------------------*/
+/*----------------------------------------------------------------------------*/
+void		print_tok_lst(t_token *lst);
+void		print_ast(t_ast_node *node, int n);
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------- LEXICAL ANALIZER -----------------------------*/
+/*----------------------------------------------------------------------------*/
+void		quote_check(char *line, int *i);
+void		check_open_close(t_shell *shell);
+void		check_brace(t_shell *shell, t_token **tok_lst);
+void		check_here_count(t_shell *shell);
+void		clear_stack(t_stack **brace);
+void		operator_input(t_token *node);
+int			get_op_type(char **s);
+t_token		*get_operator_token(char **s);
+t_input		*get_word(char **s);
+t_token		*get_word_token(char **s);
+t_token		*input_scanner(char *str);
+t_token		*get_token(char **s);
+void		token_add(t_token **tok_lst, t_token *token);
+int			operator_analyser(t_shell *shell, t_token **lst);
+int			brace_analyser(t_shell *shell, t_token **lst);
+int			redirections_analyser(t_shell *shell, t_token **lst);
+int			env_param_analizer(t_shell *shell, t_token **lst);
+int			token_analyser(t_shell *shell, t_token *tok);
+void		token_free(t_token **tok_lst);
+
+/*----------------------------------------------------------------------------*/
+/*---------------------------------- PARSER ----------------------------------*/
+/*----------------------------------------------------------------------------*/
+t_input		*new_word_node(t_token **tok_lst);
+t_redir		*new_redir_node(t_shell *shell, t_token **tok_lst);
+t_ast_node	*new_cmd_node(t_shell *shell, t_cmd *cmd, t_token **tok_lst, int n);
+void		add_cmd_node(t_token **tok_lst, t_cmd **cmd);
+t_ast_node	*line_parsing(t_shell *shell, t_token **tok_lst);
+t_ast_node	*parse_pipeline(t_shell *shell, t_token **tok_lst);
+t_ast_node	*parse_logic_op(t_shell *shell, t_ast_node *left, t_token **scan);
+t_ast_node	*parse_pipe(t_shell *shell, t_ast_node *left, t_token **tok_lst);
+t_ast_node	*parse_cmd_line(t_shell *shell, t_token **tok_lst);
+t_ast_node	*parse_subshell(t_shell *shell, t_token **tok_lst);
+t_ast_node	*parse_redir(t_shell *shell, t_token **tok_lst);
+t_input		*parse_filename(t_token **tok_lst);
+t_ast_node	*parse_simple_cmd(t_shell *shell, t_token **tok_lst);
+void		parse_heredoc(t_shell *shell, t_redir **node);
+void		free_ast(t_ast_node **node);
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------------- BUILT_IN ---------------------------------*/
+/*----------------------------------------------------------------------------*/
+void		pwd(t_shell *shell);
+int			export(t_shell *shell, t_cmd *cmd);
+int			echo(t_shell *shell, t_cmd *cmd);
+int			unset(t_shell *shell, t_cmd *cmd);
+int			cd(t_shell *shell, t_cmd *cmd);
+int			my_exit(t_shell *shell, t_cmd *cmd);
+char		*get_pid(void);
+char		*initialize_name(char **envp, int i, int j);
+char		*initialize_value(char **envp, int i, int j);
+int			add_hidden_values(t_shell *shell);
+void		get_env(t_shell *shell, char **envp);
+char		*search_var(t_env *env, char *var_name);
+void		add_env_node(int hidden, char *name, char *value, t_shell *shell);
+void		del_env_node(char *var_name, t_shell *shell);
+int			env_lenght(t_shell *shell);
+char		**env_vars(t_shell *shell);
+void		init_env(t_shell *shell);
+void		print_env(t_shell *shell);
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------------- HISTORY ----------------------------------*/
+/*----------------------------------------------------------------------------*/
+void		shell_history(t_shell *shell);
+void		print_history(t_shell *shell, t_cmd *cmd);
+
+/*----------------------------------------------------------------------------*/
+/*---------------------------------- SIGNAL ----------------------------------*/
+/*----------------------------------------------------------------------------*/
+void		sig_init(t_shell *shell);
+void		eof_handler(t_shell *shell);
+
+/*----------------------------------------------------------------------------*/
+/*--------------------------------- EXECUTE ----------------------------------*/
+/*----------------------------------------------------------------------------*/
+int			wd_match(const char *pattern, const char *text);
+char		**get_path(t_shell *shell);
+char		*find_cmd_abs_path(t_shell *shell, t_cmd *cmd);
+void		call_builtins(t_shell *shell, t_cmd *cmd);
+int			execute(t_shell *shell, t_ast_node *node);
+void		execute_log_op(t_shell *shell, t_ast_node *node);
+void		execute_pipeline(t_shell *shell, t_ast_node *node);
+
+/*----------------------------------------------------------------------------*/
+/*---------------------------------- UTILS -----------------------------------*/
+/*----------------------------------------------------------------------------*/
+void		fds_init(t_shell *shell);
+void		dup2_err(t_shell *shell);
+char		*remove_quotes(char *str);
+void		builtin_err(t_shell *shell, char *err, char *input);
+int			error(int cond, char *str, int ecode, t_shell *shell);
+void		error_exit(int cond, char *str, int ecode);
+void		set_err(t_shell *shell, char *str);
+void		set_error_stat(int stat, t_token **lst);
+char		*join_with_symbol(char *s1, char *s2, char c);
+int			set_status(t_shell *shell, int stat);
+void		search_heredoc(t_shell *shell, t_token *lst);
+void		fake_heredoc(t_shell *shell, t_token *lim);
+void		here_wait(t_shell *shell, pid_t *pid);
+void		set_attr(int mode);
+void		hd_wait(t_shell *shell, int *pid, t_redir **node);
+void		heredoc(t_shell *shell, t_input *word, t_redir *node);
+void		close_fds(t_shell *shell);
+int			is_env_name(char *s);
+void		ft_append(char **str, char c);
+char		*find_dollar(char *str, int flag);
+char		*find_env_param(t_shell *shell, char *key);
+char		*find_env_name(t_shell *shell, char *key);
+// char		*get_var_value(t_shell *shell, char *str, int *len);
+// char		*replace_with_value(t_shell *shell, char *str, char **loc);
+// char		*expand_param(t_shell *shell, char *str, int flag);
+void		expand(t_shell *shell, t_ast_node *node);
+
+#endif
