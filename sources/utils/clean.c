f19e8b788ac2e2cfc7169b623d243b160c219b1f
Marie <mari.nazaryan7173@gmail.com>
2026-06-25 14:47:55 +0400

    shell cleanup

--- sources/utils/clean.c
+++ sources/utils/clean.c
@@ -6,7 +6,7 @@
 /*   By: mnazarya <mnazarya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:49:16 by mnazarya          #+#    #+#             */
-/*   Updated: 2026/06/25 14:24:41 by mnazarya         ###   ########.fr       */
+/*   Updated: 2026/06/25 15:28:38 by mnazarya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,9 +31,9 @@ void	clean(t_shell *shell)
 		free(shell->prev);
 	if (shell->hist)
 		free(shell->hist);
-	if (shell->token_head)
+	if (shell->line && shell->token_head)
 		token_free(&(shell->token_head));
-	if (shell->tree)
+	if (shell->line && shell->tree)
 		free_ast(&(shell->tree));
 	if (shell->all_fds[0] >= 3)
 		close(shell->all_fds[0]);
